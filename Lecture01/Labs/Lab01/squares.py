05910bddacfbe1775e96b1217e37fe5fb5deca46
Duo <duochen@yahoo.com>
2020-06-08 22:10:30 -0500

    check in code

---
+++ Lecture01/Labs/Lab01/squares.py
@@ -0,0 +1,13 @@
+import turtle
+
+tony = turtle.Pen()
+for i in range(60):
+    tony.forward(100)
+    tony.right(90)
+    tony.forward(100)
+    tony.right(90)
+    tony.forward(100)
+    tony.right(90)
+    tony.forward(100)
+    tony.right(90)
+    tony.right(6)
