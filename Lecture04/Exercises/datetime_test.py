c0bd24824d08ef20115d360eca87777723bb8ac7
Duo <duochen@yahoo.com>
2020-06-27 17:08:13 -0500

    check in code

---
+++ Lecture04/Exercises/datetime_test.py
@@ -0,0 +1,13 @@
+import datetime
+
+x = datetime.datetime.now()
+print("datetime:", x)
+print("year: ", x.year)
+print("month: ", x.month)
+print("day: ", x.day)
+print("hour: ", x.hour)
+print("minute: ", x.minute)
+print("second: ", x.second)
+
+y = datetime.datetime(2020, 7, 4)
+print(y)
