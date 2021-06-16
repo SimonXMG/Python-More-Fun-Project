9e585b8a921ca6b65c9ea6d8e8dc3f9bdb3f8081
SimonXMG <66859123+SimonXMG@users.noreply.github.com>
2021-06-15 19:19:28 -0500

    Delete Lecture04/Homework/Homework01 directory

--- Lecture04/Homework/Homework01/my_module.py
+++
@@ -1,7 +0,0 @@
-import math
-
-def compute(numbers):
-    result = []
-    for n in numbers:
-        result.append(math.factorial(n))
-    return result
--- Lecture04/Homework/Homework01/use_my_module.py
+++
@@ -1,4 +0,0 @@
-from my_module import compute
-
-result = compute([5,7,11])
-print(result)
