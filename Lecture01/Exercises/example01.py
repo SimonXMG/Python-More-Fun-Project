66c476feb32408e41bbc30218247aed2128add4c
SimonXMG <66859123+SimonXMG@users.noreply.github.com>
2021-06-15 19:00:32 -0500

    Delete Lecture01/Exercises directory

--- Lecture01/Exercises/example01.py
+++
@@ -1,8 +0,0 @@
-x = 3
-
-def my_func():
-    x = 5
-    print(x)
-
-my_func()   #  5
-print(x)    #  3
--- Lecture01/Exercises/example02.py
+++
@@ -1,7 +0,0 @@
-score = 0
-
-def update_score(new_score):
-    score = new_score
-
-update_score(100)
-print(score)    # 0
